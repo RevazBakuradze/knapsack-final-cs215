637ddb640047f04e108cc3afb42c821b07054458
Aleksandr Molchagin <contact@aleksandrmolchagin.com>
2021-06-05 15:01:31 -0400

    Items sort

--- src/com/finalProject/Item.java
+++ src/com/finalProject/Item.java
@@ -1,11 +1,28 @@
 package com.finalProject;
-
+/**
+ *  CS215, Final Programming Project: Knapsack 
+ *
+ *  Item class.
+ *  
+ *  A class to store the data for each item: its original number, profit, weight, profit/weight ratio.
+ *
+ *  @author Aleksandr Molchagin
+ *  @author Revaz Bakuradze
+ *  @version June 5 2021
+ */
 public class Item {
-    private int number;
-    private int profit;
-    private int weight;
-    private double ratio;
+    private int number;     //Represents the original object's number
+    private int profit;     //Represents profit
+    private int weight;     //Represents weight
+    private double ratio;   //Represents profit/weight ratio
 
+     /**
+     * Constructs a new object of this class with parameters.
+     * 
+     *      @param   number    line's number
+     *      @param   profit    profit
+     *      @param   weight    weight
+     */
     public Item(int number, int profit, int weight) {
         this.number = number;
         this.profit = profit;
@@ -13,8 +30,12 @@ public class Item {
         if (weight != 0)
             this.ratio = ((double)profit) / ((double)weight);
         else
-            this.ratio = 0;    }
+            this.ratio = 0;    
+        }
 
+     /**
+     * Constructs a new object of this class with NO parameters.
+     */
     public Item() {
         this.number = 0;
         this.profit = 0;
@@ -25,18 +46,34 @@ public class Item {
             this.ratio = 0;
     }
 
+    /**
+     * Gets the value number.
+     *      @return number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Sets a new the value as the number.
+     *      @param number
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Gets the value of profit.
+     *      @return profit
+     */
     public int getProfit() {
         return profit;
     }
 
+    /**
+    * Sets a new the value as the profit and updates the ratio if possible.
+    *      @param profit
+    */
     public void setProfit(int profit) {
         this.profit = profit;
         if (weight != 0)
@@ -45,10 +82,18 @@ public class Item {
             this.ratio = 0;
     }
 
+     /**
+     * Gets the value of weight.
+     *      @return weight
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+    * Sets a new the value as the weight and updates the ratio if possible.
+    *      @param weight
+    */
     public void setWeight(int weight) {
         this.weight = weight;
         if (weight != 0)
@@ -57,14 +102,19 @@ public class Item {
             this.ratio = 0;
     }
 
+    /**
+     * Gets the value profit/weight ratio.
+     *      @return profit/weight ratio
+     */
     public double getRatio() {
         return ratio;
     }
 
+    /**
+     * Prints an Item including its number, profit, weight and ratio.
+     */
     @Override
     public String toString() {
-        return "Item " + number + ": Profit - " + profit + ", Weight - " + weight + ", Ratio " + ratio;
+        return "Item " + number + ": \n\tProfit: " + profit + "\n\tWeight: " + weight + "\n\tRatio: " + ratio;
     }
 }
-
-
--- src/com/finalProject/Main.java
+++ src/com/finalProject/Main.java
@@ -1,11 +1,27 @@
 package com.finalProject;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Comparator;
+/**
+ *  CS215, Final Programming Project: Knapsack 
+ *
+ *  Main Class.
+ *
+ *  DETAILED DESCRIPTION SHOULD BE UPDATED.
+ *
+ *  @author Revaz Bakuradze
+ *  @author Aleksandr Molchagin
+ *  @version June 5 2021
+ */
 public class Main {
 
+    /**
+     *  The main function initiates execution of this program.
+     *    @param    String[] args not used in this program
+     *              (but main methods always need this parameter)
+     **/
     public static void main(String[] args) throws FileNotFoundException {
 
         //The location of input file with data sample
@@ -13,44 +29,44 @@ public class Main {
 
         Scanner scanner = new Scanner(file);
 
-        //numberOfItems represents the number of items in the problem set
-        int numberOfItems = Integer.parseInt(scanner.nextLine());
-
-        //totalWeightLimit represents the total weight limit
-        int totalWeightLimit = Integer.parseInt(scanner.nextLine());
-
-        //Three arrays storing number of an item, profit, and weight
-        int[] itemNumberArray = new int[numberOfItems];
-        int[] profitArray = new int[numberOfItems];
-        int[] weightArray = new int[numberOfItems];
-
-        Item[] items = new Item[numberOfItems];
-        System.out.println("/n" + Arrays.toString(items));
+        int numberOfItems = Integer.parseInt(scanner.nextLine());       //Represents the number of items in the problem set
+        int totalWeightLimit = Integer.parseInt(scanner.nextLine());    //Represents the total weight limit
+        Item[] items = new Item[numberOfItems];                         //Represents array of all items
 
         //For loop for inputting values from file to the arrays
         for (int i = 0; i < numberOfItems; i++) {
 
-            //Scanning line
-            String line = scanner.nextLine();
+            String line = scanner.nextLine();               //Getting the next line
 
-            //Breaking up the line with spaces and storing words in array
-            String seperatedWords[] = line.split(" ", 3);
+            String seperatedWords[] = line.split(" ", 3);   //Breaking up the line with spaces and storing words in array
+
             
             int number = Integer.parseInt(seperatedWords[0]);
             int profit = Integer.parseInt(seperatedWords[1]);
             int weight = Integer.parseInt(seperatedWords[2]);
 
-            items[i] = new Item(number, profit, weight);
-            
-            System.out.println("\n\t" + items[i]);
-
+            items[i] = new Item(number, profit, weight);    //Creating a new item an put it into the array
         }
 
-        for (int i = 0; i < items.length; i++)
-            System.out.println("\n\t" + items[i]);
-        }
+        Arrays.sort(items, new Comparator<Item>() {     //Sort all items using a custom comparator (reverse sort)
+            @Override
+            public int compare(Item a, Item b) {
+                return Double.compare(b.getRatio(), a.getRatio());  //Compare based on ratios
+            }
+        });
+    
+        for (int i = 0; i < items.length; i++)          //Print all items
+            System.out.println("\n" + items[i]);
 
-        //create 3rd array having profit weight ration. swap and do swap in correspoding array
+    }//main end
+    
+}//class end
+
+/*
+    USEFUL NOTES
+
+    //create 3rd array having profit weight ration. swap and do swap in correspoding array
+    
     //when you sort her sort there too
 
     //create object and sort by ratio
@@ -60,4 +76,4 @@ public class Main {
     //6.1 new node for this alorithm implementation
 
     //root level -1 children would be 0 - first elments.
-}
+*/
