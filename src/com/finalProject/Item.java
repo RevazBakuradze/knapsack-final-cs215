82424d796bd60a370a29eafadf6c9aaf40da1c74
Aleksandr Molchagin <contact@aleksandrmolchagin.com>
2021-06-05 13:56:28 -0400

    Implemented Item object

---
+++ src/com/finalProject/Item.java
@@ -0,0 +1,70 @@
+package com.finalProject;
+
+public class Item {
+    private int number;
+    private int profit;
+    private int weight;
+    private double ratio;
+
+    public Item(int number, int profit, int weight) {
+        this.number = number;
+        this.profit = profit;
+        this.weight = weight;
+        if (weight != 0)
+            this.ratio = ((double)profit) / ((double)weight);
+        else
+            this.ratio = 0;    }
+
+    public Item() {
+        this.number = 0;
+        this.profit = 0;
+        this.weight = 0;
+        if (weight != 0)
+            this.ratio = ((double)profit) / ((double)weight);
+        else
+            this.ratio = 0;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public void setProfit(int profit) {
+        this.profit = profit;
+        if (weight != 0)
+            this.ratio = ((double)profit) / ((double)weight);
+        else
+            this.ratio = 0;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+        if (weight != 0)
+            this.ratio = ((double)profit) / ((double)weight);
+        else
+            this.ratio = 0;
+    }
+
+    public double getRatio() {
+        return ratio;
+    }
+
+    @Override
+    public String toString() {
+        return "Item " + number + ": Profit - " + profit + ", Weight - " + weight + ", Ratio " + ratio;
+    }
+}
+
+
--- src/com/finalProject/Main.java
+++ src/com/finalProject/Main.java
@@ -4,20 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //File location might change depending on the address of the file
+        //The location of input file with data sample
         File file = new File("input_data//sample.dat");
 
         Scanner scanner = new Scanner(file);
 
-        //n represents the number of items in the problem set
+        //numberOfItems represents the number of items in the problem set
         int numberOfItems = Integer.parseInt(scanner.nextLine());
 
-        //w represents the total weight limit
+        //totalWeightLimit represents the total weight limit
         int totalWeightLimit = Integer.parseInt(scanner.nextLine());
 
         //Three arrays storing number of an item, profit, and weight
@@ -25,6 +24,8 @@ public class Main {
         int[] profitArray = new int[numberOfItems];
         int[] weightArray = new int[numberOfItems];
 
+        Item[] items = new Item[numberOfItems];
+        System.out.println("/n" + Arrays.toString(items));
 
         //For loop for inputting values from file to the arrays
         for (int i = 0; i < numberOfItems; i++) {
@@ -34,17 +35,19 @@ public class Main {
 
             //Breaking up the line with spaces and storing words in array
             String seperatedWords[] = line.split(" ", 3);
+            
+            int number = Integer.parseInt(seperatedWords[0]);
+            int profit = Integer.parseInt(seperatedWords[1]);
+            int weight = Integer.parseInt(seperatedWords[2]);
 
-            itemNumberArray[i] = Integer.parseInt(seperatedWords[0]);
+            items[i] = new Item(number, profit, weight);
+            
+            System.out.println("\n\t" + items[i]);
 
-            profitArray[i] = Integer.parseInt(seperatedWords[1]);
-            weightArray[i] = Integer.parseInt(seperatedWords[2]);
         }
 
-        System.out.println(Arrays.toString(itemNumberArray));
-        System.out.println(Arrays.toString(profitArray));
-        System.out.println(Arrays.toString(weightArray));
-
+        for (int i = 0; i < items.length; i++)
+            System.out.println("\n\t" + items[i]);
         }
 
         //create 3rd array having profit weight ration. swap and do swap in correspoding array
